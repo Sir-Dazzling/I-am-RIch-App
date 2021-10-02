ac429e675c4e389e6d5374fbfb6952a66ec7491a
Otti Daniel <ottidan20@gmail.com>
2021-10-02 08:54:44 +0100

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,7 @@ void main() {
       ),
       backgroundColor: Colors.blueGrey,
       body: Center(
-        child: Image(
-            image: NetworkImage(
-                'https://images.pexels.com/photos/1987301/pexels-photo-1987301.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')),
+        child: Image(image: AssetImage('assets/images/diamond.png')),
       ),
     ),
   ));
